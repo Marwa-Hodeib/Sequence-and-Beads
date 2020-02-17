c623efb46f2b1f3a882a67c2c7d965b4fa05c0f9
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-02-17 22:11:09 +0200

    after filtering

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from "./pages/home/home";
 import About from "./pages/about/about";
-import Gallery from "./pages/gallery/gallery";
 import Contact from "./pages/contact_us /contact_us";
 
 class App extends Component {
@@ -17,7 +16,7 @@ class App extends Component {
              <Route path="/" component={Home} exact/>
              <Route path="/about" component={About}/>
              <Route path="/contact" component={Contact}/>
-             <Route path="/gallery" component={Gallery}/>
+             {/* <Route path="/gallery" component={Gallery}/> */}
            </Switch>
         </div> 
       </BrowserRouter>
