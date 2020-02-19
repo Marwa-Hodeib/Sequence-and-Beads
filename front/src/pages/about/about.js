81109343c15d5899fe7629a3566175db194ff057
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-02-19 21:43:21 +0200

    technically nothing

--- front/src/pages/about/about.js
+++ front/src/pages/about/about.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../../components/header/header";
-import Slider from "../../components/about-slider/about-slider";
 import Footer from "../../components/Footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
